a6bae47c58290f859c6b4be195295d7071a911a6
Jim Edson Cortes Torres <jicortes@c2r5s3.42malaga.com>
2022-10-30 19:33:58 +0100

    Actualizando compare.c con las reglas norminette

--- ex00/compare.c
+++ ex00/compare.c
@@ -1,70 +1,92 @@
-/*
- 1.-Los extremos opuestos de cada fila y columna deben sumar un valor comprendido en el intervalo [3, n+1].
- 2.-No puede haber esquinas (1,n), y las esquinas con  siempre son (1,1). OK
- 3.-Cada borde contiene como mínimo un 1, y como máximo un n.
- */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compare.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jicortes <jicortes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 19:08:39 by jicortes          #+#    #+#             */
+/*   Updated: 2022/10/30 19:32:37 by jicortes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-void    extremos(int size, int **clues)
+void	extremos(int size, int **clues)
 {
-    int n;
-    n = size - 1;
-    if (((clues[0][0] + clues[1][0]) < 3 || (clues[0][0] + clues[1][0]) > (size + 1)) || ((clues[0][n] + clues[1][n]) < 3 || (clues[0][n] + clues[1][n]) > (size + 1)) || ((clues[3][0] + clues[3][n]) < 3 || (clues[3][0] + clues[3][n]) > (size + 1)) || ((clues[2][0] + clues[2][n]) < 3 || (clues[2][0] + clues[2][n]) > (size + 1)) )
-    {
-        write(1, "E", 1);
-    }
+	int	n;
+
+	n = size - 1;
+	if (((clues[0][0] + clues[1][0]) < 3
+	|| (clues[0][0] + clues[1][0]) > (size + 1))
+	|| ((clues[0][n] + clues[1][n]) < 3
+	|| (clues[0][n] + clues[1][n]) > (size + 1))
+	|| ((clues[3][0] + clues[3][n]) < 3
+	|| (clues[3][0] + clues[3][n]) > (size + 1))
+	|| ((clues[2][0] + clues[2][n]) < 3
+	|| (clues[2][0] + clues[2][n]) > (size + 1)))
+		return (0);
+	return (1);
 }
 
-void    one_n(int size, int **clues)
+void	one_one(int size, int **clues)
 {
-    int n;
-    n = size - 1;
-    if(clues[0][0] == 1 && clues[2][0] != 1 || clues[0][n] == 1 && clues[3][0] != 1 || clues[2][n] == 1 && clues[1][0] != 1 || clues[3][n] == 1 && clues[1][n] != 1 || clues[0][0] != 1 && clues[2][0] == 1 || clues[0][n] != 1 && clues[3][0] == 1 || clues[2][n] != 1 && clues[1][0] == 1 || clues[3][n] != 1 && clues[1][n] == 1)
-    {
-        write(1, "E", 1);
-    }
+	int	n;
+
+	n = size - 1;
+	if (clues[0][0] == 1 && clues[2][0] != 1
+	|| clues[0][n] == 1 && clues[3][0] != 1
+	|| clues[2][n] == 1 && clues[1][0] != 1
+	|| clues[3][n] == 1 && clues[1][n] != 1 ||
+	clues[0][0] != 1 && clues[2][0] == 1
+	|| clues[0][n] != 1 && clues[3][0] == 1
+	|| clues[2][n] != 1 && clues[1][0] == 1
+	|| clues[3][n] != 1 && clues[1][n] == 1)
+		return (0);
+	return (1);
 }
 
-void    onefive(int size, int **clues)
+void	one_n(int size, int **clues)
 {
-    int n;
-    n = size - 1;
-    if(clues[0][0] == 1 && clues[2][0] == size || clues[0][n] == 1 && clues[3][0] == size || clues[2][n] == 1 && clues[1][0] == size || clues[3][n] == 1 && clues[1][n] == size || clues[0][0] == size && clues[2][0] == 1 || clues[0][n] == size && clues[3][0] == 1 || clues[2][n] == size && clues[1][0] == 1 || clues[3][n] == size && clues[1][n] == 1)
-    {
-        write(1, "E", 1);
-    }
+	int	n;
+
+	n = size - 1;
+	if (clues[0][0] == 1 && clues[2][0] == size
+	|| clues[0][n] == 1 && clues[3][0] == size
+	|| clues[2][n] == 1 && clues[1][0] == size
+	|| clues[3][n] == 1 && clues[1][n] == size
+	|| clues[0][0] == size && clues[2][0] == 1
+	|| clues[0][n] == size && clues[3][0] == 1
+	|| clues[2][n] == size && clues[1][0] == 1
+	|| clues[3][n] == size && clues[1][n] == 1)
+		return (0);
+	return (1);
 }
 
-void    minmaxborder(int size, int **clues)
+void	minmaxborder(int size, int **clues)
 {
-    int i;
-    int j;
-    int count1;
-    int count2;
-    
-    i = 0;
-    while(i < size)
-    {
-        j = 0;
-        count1 = 0;
-        count2 = 0;
-        while(j < size)
-        {
-            if(clues[i][j] == 1)
-            {
-                count1++;
-            }
-            if(clues[i][j] == size)
-            {
-                count2++;
-            }
-            j++;
-        }
-        if(count1 < 1 || count2 > 1)
-        {
-            write(1, "E", 1);
-        }
-    i++;
-    }
+	int	i;
+	int	j;
+	int	count1;
+	int	count2;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		count1 = 0;
+		count2 = 0;
+		while (j < size)
+		{
+			if (clues[i][j] == 1)
+				count1++;
+			if (clues[i][j] == size)
+				count2++;
+			j++;
+		}
+		if (count1 < 1 || count2 > 1)
+			return (0);
+		return (1);
+	i++;
+	}
 }
