6ab08f9b0f29e836505552b6aae0ee5edaceb951
Galán <antoniogalanG15@gmail.com>
2022-10-30 20:42:47 +0100

    Funciones de verificación de solución implementadas

---
+++ ex00/verify.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   verify.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 18:42:10 by antgalan          #+#    #+#             */
+/*   Updated: 2022/10/30 20:18:48 by antgalan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	check_down(int border, int **board, int col, int n)
+{
+	int	jumps;
+	int	i;
+
+	i = 0;
+	jumps = 1;
+	while (board[i][col] < n)
+	{
+		if (board[i][col] < board[i + 1][col])
+			jumps++;
+		i++;
+	}
+	if (jumps == border)
+		return (1);
+	return (0);
+}
+
+int	check_up(int border, int **board, int col, int n)
+{
+	int	jumps;
+	int	i;
+
+	i = n - 1;
+	jumps = 1;
+	while (board[i][col] < n)
+	{
+		if (board[i + 1][col] < board[i][col])
+			jumps++;
+		i--;
+	}
+	if (jumps == border)
+		return (1);
+	return (0);
+}
+
+int	check_right(int border, int **board, int row, int n)
+{
+	int	jumps;
+	int	i;
+
+	i = 0;
+	jumps = 1;
+	while (board[row][i] < n)
+	{
+		if (board[row][i] < board[row][i + 1])
+			jumps++;
+		i++;
+	}
+	if (jumps == border)
+		return (1);
+	return (0);
+}
+
+int	check_left(int border, int **board, int row, int n)
+{
+	int	jumps;
+	int	i;
+
+	i = n - 1;
+	jumps = 1;
+	while (board[row][i] < n)
+	{
+		if (board[row][i - 1] < board[row][i])
+			jumps++;
+		i--;
+	}
+	if (jumps == border)
+		return (1);
+	return (0);
+}
+
+int	is_solution(int **borders, int **board, int n)
+{
+	int	sol;
+	int	i;
+
+	sol = 1;
+	i = 0;
+	while (i < n * 1 && sol)
+		sol *= check_down(borders[0][i], board, i, n);
+	i = 0;
+	while (i < n * 2 && sol)
+		sol *= check_up(borders[1][i], board, i, n);
+	i = 0;
+	while (i < n * 3 && sol)
+		sol *= check_right(borders[2][i], board, i, n);
+	i = 0;
+	while (i < n * 4 && sol)
+		sol *= check_left(borders[3][i], board, i, n);
+	return (sol);
+}
