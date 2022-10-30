2a206f65e243421d0b0b2e5e6a06b39a0a8aafbb
Galán <antoniogalanG15@gmail.com>
2022-10-30 17:13:02 +0100

    Archivo 'main.c' modificado

--- ex00/main.c
+++ ex00/main.c
@@ -6,19 +6,22 @@
 /*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 13:18:24 by antgalan          #+#    #+#             */
-/*   Updated: 2022/10/30 11:17:57 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/10/30 17:10:24 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "inputs.h"
+#include "board.h"
 
 int	main(int args, char **argv)
 {
-	if (args != 2)
+	int	**board;
+	if (args != 2 || !is_valid_input(argv[1]))
+	{
 		write(1, "Error\n", 6);
-	else if (!is_valid_input(argv[1]))
-		write(1, "Error\n", 6);
+		return (0);
+	}
 	else
 		write(1, "Valid\n", 6);
 	return (0);
