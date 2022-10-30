9956342709dac63db3f349dec9fe8f4fdc85e33f
Galán <antoniogalanG15@gmail.com>
2022-10-30 09:32:52 +0100

    Archivo de cabeceras de 'inputs.c' creado
    Se movieron los comentarios de documentación.

--- ex00/inputs.c
+++ ex00/inputs.c
@@ -6,44 +6,20 @@
 /*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 13:02:07 by antgalan          #+#    #+#             */
-/*   Updated: 2022/10/29 19:26:17 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/10/30 09:19:02 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/**
- * @brief   Checks if a character is a number.
- * 
- * @param c     Character to check.
- * 
- * @return  '1' if it's a character; '0' otherwise.
- */
 int	is_number(char c)
 {
 	return ('0' <= c && c <= '9');
 }
 
-/**
- * @brief   Checks if a column/row value is a valid
- *          one for a 'nxn' instance of the problem.
- * 
- * @param num   Value to check.
- * @param n     Problem dimension.
- * 
- * @return  '1' if valid; '0' otherwise.
- */
 int	is_valid_number(char num, int n)
 {
 	return ('1' <= num && num <= ('0' + n));
 }
 
-/**
- * @brief	Calculates the 'n' value from 'nxn' dimension of a problem.
- * 			'n' must be strictly positive.
- * 
- * @param input		The 'string' to analyze.
- * 
- * @return	'n' value, if possible; '0' otherwise;
-*/
 int	calculate_n(char *input)
 {
 	int	x;
@@ -68,15 +44,6 @@ int	calculate_n(char *input)
 	return (0);
 }
 
-/**
- * @brief	Checks if an argument structure is valid.
- * 			Estructure must be "n1 n2 n3 n4 n5 ...",
- * 			numbers followed by spaces, except last one.
- * 
- * @param input	Argument to check.
- * 
- * @return '1' if it's valid; '0' otherwise.
- */
 int	is_valid_structure(char *input)
 {
 	int	valid;
@@ -96,14 +63,6 @@ int	is_valid_structure(char *input)
 	return (valid);
 }
 
-/**
- * @brief   Checks if an input is valid.
- * 
- * @param input	Input to check.
- * @param n     Problem dimension.
- * 
- * @return  '1' if valid; '0' otherwise.
- */
 int	valid_input(char *input)
 {
 	int	n;
